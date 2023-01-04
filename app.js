33f190e4272ac3de8681550d134394c6740d31a0
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-01-04 14:55:24 -0300

    corrigindo o erro no app

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Local de armazenamento de games")
 console.log("para jogar e se divertir com amigos")
+console.log("novos contatos")
